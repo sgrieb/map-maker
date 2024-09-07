7f4ccbfae9a9daa3a767931e8d104aae0def3e32
Steve Grieb <steve@openthrottle.consulting>
2024-09-07 18:24:33 -0400

    Returning errors works

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,12 +1,29 @@
 'use server'
- 
-export async function createList(userId: string, formData: FormData) {
+
+import { z } from 'zod'
+
+const schema = z.object({
+    name: z.string().min(1, 'Name is required.'),
+    description: z.string().min(1, 'Description is required.'),
+})
+
+export async function createList(prevState: any, formData: FormData) {
     const rawFormData = {
-      name: formData.get('listName'),
-      description: formData.get('listDescription'),
+        name: formData.get('listName'),
+        description: formData.get('listDescription'),
     }
- 
-    console.log(`got form data: ${JSON.stringify(rawFormData)}`)
-    // mutate data
-    // revalidate cache
-  }
+
+    const validatedFields = schema.safeParse(rawFormData)
+
+    if (!validatedFields.success) {
+        let errors: any = {}
+
+        validatedFields.error.issues.forEach((issue) => {
+            errors[issue.path[0]] = issue.message
+        })
+
+        formData.append('errors', JSON.stringify(errors))
+    }
+
+    return formData
+}
--- src/app/list/add/page.tsx
+++ src/app/list/add/page.tsx
@@ -1,29 +1,52 @@
-'use client'
- 
-import { createList } from '@/app/actions'
+"use client";
+
+import { createList } from "@/app/actions";
+import { useFormState, useFormStatus } from 'react-dom';
 
 export default function Page() {
-  const userId = '1234'
-  const createListWithId = createList.bind(null, userId)
+  const { pending } = useFormStatus()
+  const [state, formAction] = useFormState(createList, new FormData())
+
+  const userId = "1234";
+  let errors = null
+
+  if(Array.isArray(state)) {
+    let errorField = state.find((formItem) => {
+      return formItem[0] == 'errors'
+    })
+
+    errors = JSON.parse(errorField[1])
+    debugger
+  }
 
   return (
     <div>
       <h1>Add a List</h1>
-      <form action={createListWithId}>
+      <form action={formAction}>
         <label className="m-auto mt-6 form-control w-full max-w-xs">
-        <div className="label">
+          <div className="label">
             <span className="label-text">Name</span>
-        </div>
-        <input className="input input-bordered w-full max-w-xs" type="text" />
+          </div>
+          <input
+            name="listName"
+            className="input input-bordered w-full max-w-xs"
+            type="text"
+          />
         </label>
         <label className="m-auto mt-6 form-control w-full max-w-xs">
-        <div className="label">
+          <div className="label">
             <span className="label-text">Description</span>
-        </div>
-        <input className="input input-bordered w-full max-w-xs" type="text" />
+          </div>
+          <input
+            name="listDescription"
+            className="input input-bordered w-full max-w-xs"
+            type="text"
+          />
         </label>
         <div className="mt-6">
-        <button type="submit" className="btn btn-primary">Add</button>
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
         </div>
       </form>
     </div>
